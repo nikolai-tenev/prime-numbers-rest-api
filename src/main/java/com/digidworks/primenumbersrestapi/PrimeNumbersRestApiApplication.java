9fffca47fbf948d706d9662655480ff48cf61870
bh2o <tenev.bh2o@gmail.com>
2020-04-22 17:34:05 +0300

    Add Spring Boot.

---
+++ src/main/java/com/digidworks/primenumbersrestapi/PrimeNumbersRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.digidworks.primenumbersrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumbersRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumbersRestApiApplication.class, args);
+	}
+
+}
